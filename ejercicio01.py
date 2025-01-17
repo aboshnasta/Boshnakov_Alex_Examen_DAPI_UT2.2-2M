9fd3ec1ddbac0d1679f65af3ff469ec32ac603a3
aboshnasta <aboshnasta1@educacion.navarra.es>
2025-01-17 09:42:55 +0100

    todos

---
+++ ejercicio01.py
@@ -0,0 +1,16 @@
+diccionario=()
+while True:
+    alumnos_2M=int(input)("Elige una opcion \n"
+                          "[1] Añadir alumno/a \n"
+                          "[2] Numero de aprobados \n"
+                          "[3] Mostrar todo el menu" )
+    if alumnos_2M == ("1"):
+        print("¿Como te llamas")
+    elif alumnos_2M == ("1"):
+        print("¿Aprobado o Suspendidio?")
+    elif alumnos_2M == ("1"):
+         ("añadir alumnos_2M in diccionario")
+    if alumnos_2M == ("2"):
+        print("")
+    if alumnos_2M ==("3"):
+         alumnos_2M()
---
+++ ejercicio02.py
@@ -0,0 +1,4 @@
+def OrdenarLista(Lista):
+    lista=str["1","2","3","4","5"]
+    lista= open
+"ListaOrdenada.txt"()
